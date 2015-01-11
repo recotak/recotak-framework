a9a54157ee15dd0b48bda3b91d05ee531e41a0f7
dash <dash@recotak.org>
2015-01-11 18:47:10 +0100

    added functionaility to install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+#01/11/2015
 #
+rclient="recotak"
+rdaemon="recotakd"
+cwd=`pwd`
 
 echo "Installing recotak client"
-cd recotak
+cd $rclient
 ./install.py --all --no-confirm
-cd ..
+cd $cwd
 
 echo "Installing recotak daemon"
-cd recotakd
+cd $rdaemon
 ./install.py --all --no-confirm
-cd ..
+cd $cwd
+
+#the runtime created password for the recotak superuser
+echo "Username and password of the superuser:"
+passw=`cat $rdaemon/recotak_credentials|grep Password|sed -s 's/Password: //g'`
+
+echo "Want to make changes as superuser?"
+echo "Login with:"
+echo "recotak_client.py -a recotak:$passw 127.0.0.1:2401"
+
+echo "Create a normal user for recotak usage"
+echo "Username:"
+read user
+echo "Password:"
+read upass
+
+#create config
+cp recotak/config/create.conf install.conf
+sed -i 's/username=a/username='$user'/g' install.conf
+sed -i 's/password=a/password='$upass'/g' install.conf
+
+#add user
+recotak_client.py -a recotak:$passw -c install.conf 127.0.0.1:2401
 
 echo "Done"
